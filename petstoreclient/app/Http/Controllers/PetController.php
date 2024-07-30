6d188f9e55e5027c76be9c13470af7955cd7c728
macest77 <macest77@gmail.com>
2024-07-30 20:34:20 +0200

    add PetController base

---
+++ petstoreclient/app/Http/Controllers/PetController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+    
+    public function index()
+    {
+        
+    }
+    
+    public function addPet()
+    {
+        
+    }
+    
+    public function getPet(int $id)
+    {
+        
+    }
+    
+    public function updatePet()
+    {
+        
+    }
+    
+}
